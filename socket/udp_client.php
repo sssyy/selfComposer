44bf97afe9ad2af74ec779d8ab46e282f105dc9f
joshua <sunyakun1202@gmail.com>
2017-10-30 11:16:39 +0800

    test

--- socket/udp_client.php
+++ socket/udp_client.php
@@ -1,7 +1,7 @@
 <?php
 $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-$msg = 'hello';
-$len = strlen($msg);
-socket_sendto($sock, $msg, $len, 0, '202.85.218.133', 11109);
+$msg =(string) fopen("test.txt","r");
+$len = filesize("test.txt");
+socket_sendto($sock, $msg, $len, 0, '10.237.72.41', 11109);
 socket_close($sock);
 ?>
